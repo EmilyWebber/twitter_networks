3ec3d1e47af447c332153b9f5e6e4cd2c6f3c2de
BridgitD <bridgit.donnelly@gmail.com>
2015-05-31 23:06:42 +0000

    harvesting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import twitter
 import sys
 import json
-import pymongo
+import pymongo as pm
 
 #######################
 ## AUTHORIZE TWITTER ##
@@ -51,25 +51,35 @@ def twitter_search(twitter_api, query, max_results=200, **kw):
     return statuses
 
 
-def firehose(twitter_api, query):
+def firehose(twitter_api, write_db, query):
     twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
     stream = twitter_stream.statuses.filter(track=query)
 
     for tweet in stream:
-        print tweet['text']
+        save_to_mongo(tweet, write_db, query)
+        print tweet['user']['screen_name']
+        #output = tweet['user']['screen_name']
+        #print type(output), ' : ', output
+        #return tweet
 
 ######################
 ## TWITTER TO MONGO ##
 ######################
+def get_cursor_contents(cursor, tweet_key, component, dict_key):
+    rv = {dict_key : []}
+    for data in cursor:
+         rv[dict_key].append(data[tweet_key][component])
+    return rv
+
 
 def save_to_mongo(data, mongo_db, mongo_db_coll, **mongo_conn_kw):
-    client = pymongo.MongoClient(**mongo_conn_kw)
+    client = pm.MongoClient(**mongo_conn_kw)
     db = client[mongo_db]
     coll = db[mongo_db_coll]
     return coll.insert(data)
 
 def load_from_mongo(mongo_db, mong_db_coll, return_cursor=False, criteria=None, projection=None, **mongo_conn_kw):
-    client = pymongo.MongoClient(**mongo_conn_kw)
+    client = pm.MongoClient(**mongo_conn_kw)
     db = client[mongo_db]
     coll = db[mongo_db_coll]
 
@@ -87,30 +97,56 @@ def load_from_mongo(mongo_db, mong_db_coll, return_cursor=False, criteria=None, 
         return [ item for item in cursor ]
 
 def many_to_mongodb(tweets, db, collection):  # probablly need **kwargs
-    client = pymongo.MongoClient()
+    client = pm.MongoClient()
     database = client[db]
     mongo_collection = database[collection]
-    return mongo_collection.insert_many(tweets)
+    mongo_collection.insert_many(tweets)
 
 def get_from_mongodb(db, collection):
-    client = pymongo.MongoClient()
+    client = pm.MongoClient()
     database = client.db
     mongo_collection = database.collection
-    #print mongo_collection.distinct('retweet_count')
-    return mongo_collection.find()
+    print mongo_collection.distinct('retweet_count')
+    #return mongo_collection.find()
+
+
+
+#################
+## MONGO TESTS ##
+#################
+
+def test_print(tweets, database, collection):
+    client = pm.MongoClient()
+    db = client[database]
+    print db
+#    coll = database[collection]
+#    cursor = coll.find()
+#    for document in cursor:
+#        print document
 
 #----------------------
 
 if __name__ == '__main__':
     keys = parse_keys(sys.argv[1])
     query = sys.argv[2]
-    twitter_api = oauth_login(keys)
-    
-#    firehose(twitter_api, query)
+    write_db = sys.argv[3]
 
-    results = twitter_search(twitter_api, query, max_results=10)
+    twitter_api = oauth_login(keys)
+ 
+    firehose(twitter_api, write_db, query)
+
+''' 
+    data = get_cursor_contents(firehose(twitter_api, query), 'user',
+                               'screen_name', 'screen_name')
+ 
+    save_to_mongo(data, write_db, query)
+'''
+    #results = twitter_search(twitter_api, query, max_results=10)
+#    print results
 #    save_to_mongo(results, 'test', 'test')
 #    load_from_mongo('test', 'test')
-    print many_to_mongodb(results, 'testdb', 'testcoll')
-#    cursor = get_from_mongodb('testdb','testcoll')
+#    many_to_mongodb(results, 'testdb', 'testcoll')
+#    get_from_mongodb('testdb','testcoll')
 #    print [ item for item in cursor ]
+
+    #test_print(results, 'test', 'messi')
